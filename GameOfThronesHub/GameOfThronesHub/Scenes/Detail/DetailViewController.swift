8f985d900b1bcc83ddb0a88527cf58dff1422bf8
Bengi Anıl <bengianil34@gmail.com>
2024-09-11 17:43:03 +0300

    Created detail view files

---
+++ GameOfThronesHub/GameOfThronesHub/Scenes/Detail/DetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewController.swift
+//  GameOfThronesHub
+//
+//  Created by Bengi Anıl on 10.09.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
---
+++ GameOfThronesHub/GameOfThronesHub/Scenes/Detail/DetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewModel.swift
+//  GameOfThronesHub
+//
+//  Created by Bengi Anıl on 10.09.2024.
+//
+
+import Foundation
+
+class DetailViewModel {
+    
+}
